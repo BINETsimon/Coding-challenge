966db2cb2ab3f4f7efc86c835b4322547a4c64e6
romane77 <48509612+romane77@users.noreply.github.com>
2019-12-13 16:14:33 +0100

    dd

--- js/Script.js
+++ js/Script.js
@@ -5,20 +5,24 @@ function Verify(which){
     var S2 = document.getElementById('two').innerHTML;
     var S3 = document.getElementById('three').innerHTML;
                 
-    var uno =  S1.style.color.white;
-    var dos =  S2.style.color.white;
-    var tres = S3.style.color.white;
+    
+    
+   
 
     switch(which){
         case 1:
             if ((login.length != 0) || (modp.length != 0)){
                 if((modp.length <6) || (modp.length >12)){
                     alert('Votre mot de passe doit faire être compris entre 6 et 12 caractères');
+                    S2.style.color = "red";
                 }else{
                     alert('bravo tout est parfait !');
                 }
             }else{
                 alert('Tous les champs ne sont pas remplis, réessayez.');
+                S2.style.color = "red";
+                S3.style.color = "red";
+                S1.style.color = "red";
             }
             break;
 
@@ -29,11 +33,15 @@ function Verify(which){
             if ((login.length != 0) || (modp.length != 0) || (mail.length != 0)){
                 if((modp.length <6) || (modp.length >12)){
                     alert('Votre mot de passe doit faire être compris entre 6 et 12 caractères');
+                    S2.style.color = "red";
                 }else{
                     alert('Parfait vous êtes inscrits !');  
                 }
             }else{
                 alert('Tous les champs ne sont pas remplis réessayez');
+                2.style.color = "red";
+                S3.style.color = "red";
+                S1.style.color = "red";
             }
             break;
     }
